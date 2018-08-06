915f4c0ef44764eee9bdc37d706916c8b9f8a386
Pedro Fortunato <est.pedro.fortunato@cieemg.bhz>
2018-08-06 16:52:50 -0300

    Primeiro commit

---
+++ App.js
@@ -0,0 +1,244 @@
+import React, {Component} from 'react';
+
+import {
+  
+} from 'react-native';
+
+export class CIEEMG_APP extends Component{
+  render(){
+    return(
+      
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const botaoPressionado = () => {
+//   let numero_aleatorio = Math.random();
+//   numero_aleatorio = Math.floor(numero_aleatorio * 5);
+
+//   let frases = Array();
+//   frases[0] = 'aa';
+//   frases[1] = 'bb';
+//   frases[2] = 'cc';
+//   frases[3] = 'dd';
+//   frases[4] = 'ee';
+
+//   let fraseEscolhida = frases[numero_aleatorio];
+
+//   Alert.alert(fraseEscolhida);
+
+// }
+
+
+
+// export default props => {
+//   const {principal, botao, textoBotao} = Estilos;
+//   return(
+//     <View style={principal}>
+      
+//       <Image source={require('./imagens/Logo.png')} />
+
+//       <TouchableOpacity onPress={botaoPressionado} style={botao}>
+//         <Text style={textoBotao}>Nova frase</Text>
+//       </TouchableOpacity>  
+     
+      /* <Text style={conteudo1}>Topo</Text>
+      <TouchableOpacity style={botao} onPress={botaoPressionado}>
+        <Text style={textoBotao}>Clique aqui</Text>
+      </TouchableOpacity>
+      <Button 
+        onPress={botaoPressionado}
+        title="Clique aqui"
+        color="#845687"
+        accessibilityLabel="Clique para se cadastrar"
+      />
+      <Text style={conteudo2}>Conteúdo</Text> 
+      <Text style={conteudo3}>Rodapé</Text> */
+//     </View>
+//   );
+// }
+
+
+
+
+
+// const Estilos = {
+
+//   principal:{
+//     paddingTop: 50,
+//     backgroundColor: '#fff',
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center'
+
+//   },
+
+//   botao:{
+//     backgroundColor: '#538530',
+//     paddingVertical: 10,
+//     paddingHorizontal: 40,
+//     marginTop: 20
+//   },
+
+//   textoBotao:{
+//     color: 'white',
+//     fontSize: 16,
+//     fontWeight: 'bold'
+//   },
+    
+  // botao:{
+  //   backgroundColor: '#fff',
+  //   padding: 10,
+  //   borderColor: '#1d8eb8',
+  //   borderWidth: 1,
+  //   borderRadius: 8
+  // },
+  // textoBotao:{
+  //   color: '#48BBEC',
+  //   fontSize: 16,
+  //   fontWeight: 'bold',
+  //   alignSelf: 'center',
+
+  // },
+
+ /* -------------- Proporções com FlexBox  ------------*/
+ 
+//   principal: {
+//     backgroundColor: 'cornflowerblue',
+//     flex: 1
+//  },
+//   conteudo1: {
+//     backgroundColor: 'brown',
+//     flex: 1
+//  },
+//   conteudo2: {
+//     backgroundColor: 'yellowgreen',
+//     flex: 1
+//   },
+//   conteudo3: {
+//     backgroundColor: 'orangered',
+//     flex: 1
+//   },
+ 
+ 
+ 
+ 
+ 
+ /*  -------------Alinhamento e direcionamento com FlexBox------------ */
+  // estiloTexto: {
+  //   fontSize: 30,
+  //   backgroundColor: '#08509B',
+  //   // height: 60,
+  //   // width: 60  
+  //   //fontStyle: 'italic',
+  //   //fontWeight: '100', //valores entre 100 e 900 ou bold
+  //   //textAlign: 'center',
+  //  //textDecorationLine: 'underline line-through'
+  // },
+  // estiloView: {
+  //   backgroundColor: 'skyblue',
+  //   height: 300,
+  //   justifyContent: 'space-around',
+  //   alignItems: 'stretch', // serve para o justifyContent, flex-start, flex-end e center 
+  //   flexDirection: 'column', // usar 'row' ou 'column'
+  // },
+  // estiloTexto2: {
+  //   fontSize: 30,
+  //   backgroundColor: '#2A48FA',
+  //   // height: 60,
+  //   // width: 60  
+  // },
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const NumeroAleatorio = () => {
+//   let numero_aleatorio = Math.random();
+//   numero_aleatorio = Math.floor((numero_aleatorio * 10));
+//   alert(numero_aleatorio);
+// }
+
+
+// export default props =>(
+  
+//     <View style={{marginTop: 50}}>
+//       <Text>Meu primeiro App</Text>
+//       <Button title='Gera número randômico' onPress={NumeroAleatorio}></Button>
+//     </View>
+// )
+
+
